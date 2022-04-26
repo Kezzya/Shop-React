163364124cb571a78483d49ec6a3274654fa9410
Nikita <kobzevnikita0@mail.ru>
2022-04-26 19:33:54 +0300

    Adding Route

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Directory from "./components/directory/directory.component.jsx";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Directory />
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Directory />}></Route>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
